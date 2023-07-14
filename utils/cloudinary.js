008acb87eaafd8fc85fc9bf29c783ba59f6d6f1f
Rohit <rk22012001@gmail.com>
2023-07-14 19:41:46 +0530

    cloudinary to imagekit

--- utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import cloudinary from "cloudinary";
-
-cloudinary.config({
-  cloud_name: "octivion",
-  api_key: process.env.CLOUDINARY_API,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-export default cloudinary;
